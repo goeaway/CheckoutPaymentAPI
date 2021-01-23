cdb64e781550678bf77c1ce3a6c627384d0df7c9
Joe Thompson <joseph.thompson>
2021-01-23 14:33:27 +0000

    updated swagger ui options to not allow "try it out" on docs because they'll always return 401

--- API/CheckoutPaymentAPI/Startup.cs
+++ API/CheckoutPaymentAPI/Startup.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -21,16 +20,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Serilog;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace CheckoutPaymentAPI
 {
@@ -127,6 +124,7 @@ namespace CheckoutPaymentAPI
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "CheckoutPaymentAPI");
+                c.SupportedSubmitMethods(new SubmitMethod[] { });
             });
 
             app.UseRouting();
