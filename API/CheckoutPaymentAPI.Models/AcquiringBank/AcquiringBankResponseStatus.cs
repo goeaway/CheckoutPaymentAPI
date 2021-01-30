9e1ecb07d9a9c5baa637ceab4744621d2f368274
Joe Thompson <joseph.thompson>
2021-01-30 21:50:23 +0000

    feedback changes

---
+++ API/CheckoutPaymentAPI.Models/AcquiringBank/AcquiringBankResponseStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutPaymentAPI.Models.AcquiringBank
+{
+    public enum AcquiringBankResponseStatus
+    {
+        Success,
+        Payment_Declined,
+        Acquiring_Bank_Unreachable
+    }
+}
---
+++ API/CheckoutPaymentAPI.Models/MonthYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutPaymentAPI.Models
+{
+    public class MonthYear
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
